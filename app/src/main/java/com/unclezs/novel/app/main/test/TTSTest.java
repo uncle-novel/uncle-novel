c313ebd0981552d61552220fde227f469ba46f12
unclezs <unclezs@qq.com>
2021-06-25 02:17:02 +0800

    feat: V5.10
    1.修复书源编辑器中权重无法编辑问题
    2.修复预处理脚本部分异常问题

--- app/src/main/java/com/unclezs/novel/app/main/test/TTSTest.java
+++ app/src/main/java/com/unclezs/novel/app/main/test/TTSTest.java
@@ -1,5 +1,6 @@
 package com.unclezs.novel.app.main.test;
 
+import com.unclezs.novel.analyzer.request.RequestParams;
 import com.unclezs.novel.analyzer.util.GsonUtils;
 
 /**
@@ -9,20 +10,6 @@ import com.unclezs.novel.analyzer.util.GsonUtils;
 public class TTSTest {
 
   public static void main(String[] args) {
-    String src = "function getHtmlParas(str) {\n"
-      + "\tvar sid = str.split(\"-\");\n"
-      + "\tvar n = sid.length;\n"
-      + "\tvar vid = sid[n - 1].split(\".\")[0];\n"
-      + "\tvar pid = 0;\n"
-      + "\tvid = vid - 1;\n"
-      + "\treturn [pid, vid]\n"
-      + "}\n"
-      + "var params = getHtmlParas(url);\n"
-      + "var jsUrl = utils.absUrl(url, result);\n"
-      + "var dataJs = utils.get(jsUrl);\n"
-      + "dataJs = dataJs.replaceAll(\",urlinfo.+?;\", \";result = VideoListJson;\");\n"
-      + "var VideoListJson = eval(dataJs);\n"
-      + "result = VideoListJson[params[0]][1][params[1]].split(\"$\")[1];";
-    System.out.println(GsonUtils.me().toJson(src));
+    System.out.println(GsonUtils.NULL_PRETTY.toJson(new RequestParams()));
   }
 }
